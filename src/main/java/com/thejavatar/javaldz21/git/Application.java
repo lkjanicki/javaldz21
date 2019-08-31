b77ddfd7390d920c8278c6700c5cf2b7624f6afe
bagm2019 <bagm@o2.pl>
2019-08-31 14:59:31 +0200

    poprawka bledu

--- src/main/java/com/thejavatar/javaldz21/git/Application.java
+++ src/main/java/com/thejavatar/javaldz21/git/Application.java
@@ -6,10 +6,10 @@ public class Application {
 
     public static void main( String[] args ) {
         Scanner myObj = new Scanner( System.in );
-        System.out.println( "Wprowadz nazwe uzytkownika" );
+        System.out.println("Wprowadz nazwe uzytkownika");
 
-        String userName = myObj.nextLine(); //
-        System.out.println( "Uzytkownik wybral nazwe: " );
+        String userName = myObj.nextLine();//
+        System.out.println("Uzytkownik wybral nazwe: " + userName);
     }
 
 }
