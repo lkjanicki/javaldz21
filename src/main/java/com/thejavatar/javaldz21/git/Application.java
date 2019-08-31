9156df23a1e4910251b87ba0e8e2e64bc68062bc
[Bartosz Bala] <[bala.bartosz@gmail.com]>
2019-08-31 14:59:21 +0200

    Poprawka bledu

--- src/main/java/com/thejavatar/javaldz21/git/Application.java
+++ src/main/java/com/thejavatar/javaldz21/git/Application.java
@@ -9,7 +9,7 @@ public class Application {
         System.out.println( "Wprowadz nazwe uzytkownika" );
 
         String userName = myObj.nextLine(); //
-        System.out.println( "Uzytkownik wybral nazwe: " );
+        System.out.println( "Uzytkownik wybral nazwe: "+userName);
     }
 
 }
